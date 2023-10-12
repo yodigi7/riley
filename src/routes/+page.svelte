ae8ad8a8436cc4983b311ad62a242f499552e772
Anthony Buchholz <anthony.buchholz@gmail.com>
2023-10-12 14:54:45 -0500

    update for main page and initial animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,174 @@
-Amazing Beautiful Wonderful Lovely Incredible Gorgeous Special Kind Sweet Perfect Adorable Charming
-Caring Extraordinary Exceptional Unique Fantastic Magnificent Precious Irreplaceable
+<section
+	class="h-full flex flex-wrap place-items-center justify-center content-center text-xl"
+	style="height: 100vh;"
+>
+	<div>Riley&nbsp</div>
+	<div>is&nbsp</div>
+	<div class="words text-8xl text-center" style="flex-basis: 100%;">
+		<div>amazing</div>
+		<div>beautiful</div>
+		<div>wonderful</div>
+		<div>lovely</div>
+		<div>incredible</div>
+		<div>gorgeous</div>
+		<div>special</div>
+		<div>kind</div>
+		<div>sweet</div>
+		<div>perfect</div>
+		<div>adorable</div>
+		<div>funny</div>
+		<div>caring</div>
+		<div>extraordinary</div>
+		<div>exceptional</div>
+		<div>unique</div>
+		<div>fantastic</div>
+		<div>magnificent</div>
+		<div>precious</div>
+		<div>irreplaceable</div>
+	</div>
+</section>
+
+<style>
+	:root {
+		--text-height: 150.2px;
+	}
+	.text-8xl {
+		line-height: normal;
+	}
+	section {
+		background-color: black;
+		color: white;
+	}
+	.words {
+		height: var(--text-height);
+		overflow: hidden;
+	}
+	.words div {
+		padding-bottom: 23px;
+		opacity: 1;
+		animation: move 60s linear infinite;
+		transform: translate(0, -60px);
+	}
+	@keyframes move {
+		0% {
+			transform: translate(0, 0);
+		}
+		4% {
+			transform: translate(0, 0);
+		}
+		5% {
+			transform: translate(0, calc(var(--text-height) * -1));
+		}
+		9% {
+			transform: translate(0, calc(var(--text-height) * -1));
+		}
+		10% {
+			transform: translate(0, calc(var(--text-height) * -2));
+		}
+		14% {
+			transform: translate(0, calc(var(--text-height) * -2));
+		}
+		15% {
+			transform: translate(0, calc(var(--text-height) * -3));
+		}
+		19% {
+			transform: translate(0, calc(var(--text-height) * -3));
+		}
+		20% {
+			transform: translate(0, calc(var(--text-height) * -4));
+		}
+		24% {
+			transform: translate(0, calc(var(--text-height) * -4));
+		}
+		25% {
+			transform: translate(0, calc(var(--text-height) * -5));
+		}
+		29% {
+			transform: translate(0, calc(var(--text-height) * -5));
+		}
+		30% {
+			transform: translate(0, calc(var(--text-height) * -6));
+		}
+		34% {
+			transform: translate(0, calc(var(--text-height) * -6));
+		}
+		35% {
+			transform: translate(0, calc(var(--text-height) * -7));
+		}
+		39% {
+			transform: translate(0, calc(var(--text-height) * -7));
+		}
+		40% {
+			transform: translate(0, calc(var(--text-height) * -8));
+		}
+		44% {
+			transform: translate(0, calc(var(--text-height) * -8));
+		}
+		45% {
+			transform: translate(0, calc(var(--text-height) * -9));
+		}
+		49% {
+			transform: translate(0, calc(var(--text-height) * -9));
+		}
+		50% {
+			transform: translate(0, calc(var(--text-height) * -10));
+		}
+		54% {
+			transform: translate(0, calc(var(--text-height) * -10));
+		}
+		55% {
+			transform: translate(0, calc(var(--text-height) * -11));
+		}
+		59% {
+			transform: translate(0, calc(var(--text-height) * -11));
+		}
+		60% {
+			transform: translate(0, calc(var(--text-height) * -12));
+		}
+		64% {
+			transform: translate(0, calc(var(--text-height) * -12));
+		}
+		65% {
+			transform: translate(0, calc(var(--text-height) * -13));
+		}
+		69% {
+			transform: translate(0, calc(var(--text-height) * -13));
+		}
+		70% {
+			transform: translate(0, calc(var(--text-height) * -14));
+		}
+		74% {
+			transform: translate(0, calc(var(--text-height) * -14));
+		}
+		75% {
+			transform: translate(0, calc(var(--text-height) * -15));
+		}
+		79% {
+			transform: translate(0, calc(var(--text-height) * -15));
+		}
+		80% {
+			transform: translate(0, calc(var(--text-height) * -16));
+		}
+		84% {
+			transform: translate(0, calc(var(--text-height) * -16));
+		}
+		85% {
+			transform: translate(0, calc(var(--text-height) * -17));
+		}
+		89% {
+			transform: translate(0, calc(var(--text-height) * -17));
+		}
+		90% {
+			transform: translate(0, calc(var(--text-height) * -18));
+		}
+		94% {
+			transform: translate(0, calc(var(--text-height) * -18));
+		}
+		95% {
+			transform: translate(0, calc(var(--text-height) * -19));
+		}
+		100% {
+			transform: translate(0, calc(var(--text-height) * -19));
+		}
+	}
+</style>
