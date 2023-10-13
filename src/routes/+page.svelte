b2bcc5ea5b1af06b6bbd25ec37a1a8e99d717411
Anthony Buchholz <anthony.buchholz@gmail.com>
2023-10-12 23:23:45 -0500

    Have multiple animations working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,174 +1,123 @@
-<section
-	class="h-full flex flex-wrap place-items-center justify-center content-center text-xl"
-	style="height: 100vh;"
->
-	<div>Riley&nbsp</div>
-	<div>is&nbsp</div>
-	<div class="words text-8xl text-center" style="flex-basis: 100%;">
-		<div>amazing</div>
-		<div>beautiful</div>
-		<div>wonderful</div>
-		<div>lovely</div>
-		<div>incredible</div>
-		<div>gorgeous</div>
-		<div>special</div>
-		<div>kind</div>
-		<div>sweet</div>
-		<div>perfect</div>
-		<div>adorable</div>
-		<div>funny</div>
-		<div>caring</div>
-		<div>extraordinary</div>
-		<div>exceptional</div>
-		<div>unique</div>
-		<div>fantastic</div>
-		<div>magnificent</div>
-		<div>precious</div>
-		<div>irreplaceable</div>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let timeout;
+	let currentWordIndex = 0;
+	let words: HTMLCollection | undefined;
+	let undoAnimation = [{ opacity: 1 }, { opacity: 0 }];
+	const lingerDuration = 6000;
+	let animations = [
+		{
+			animation: [{ opacity: 0 }, { opacity: 1 }],
+			duration: 2000,
+			undoAnimation: [{ opacity: 1 }, { opacity: 0 }]
+		},
+		{
+			animation: {
+				opacity: [1, 1],
+				transform: ['translate(-50%, 0%) scale(0)', 'translate(-50%, 0%) scale(1)']
+			},
+			duration: 750,
+			undoAnimation: [{ opacity: 1 }, { opacity: 0 }]
+		},
+		{
+			animation: {
+				transform: ['translate(-50%, 0) rotateX(90deg)', 'translate(-50%, 0)'],
+				opacity: [1, 1]
+			},
+			duration: 750,
+			undoAnimation: [{ opacity: 1 }, { opacity: 0 }]
+		},
+		{
+			animation: {
+				opacity: [1, 1],
+				transform: [
+					'translate(-50%, 0) scale(0) rotate(180deg)',
+					'translate(-50%, 0) scale(1) rotate(0)'
+				]
+			},
+			duration: 750,
+			undoAnimation: [{ opacity: 1 }, { opacity: 0 }]
+		}
+	];
+
+	onMount(() => {
+		words = document.getElementById('words')?.children ?? new HTMLCollection();
+		currentWordIndex = 0;
+		timeout = setTimeout(doAnimation, lingerDuration);
+		console.log(words);
+	});
+	function getRandomAnimation() {
+		return animations[Math.floor(Math.random() * animations.length)];
+	}
+	function doAnimation() {
+		if (!words) {
+			return;
+		}
+		words?.item(currentWordIndex)?.animate(undoAnimation, {
+			duration: 2000,
+			fill: 'forwards',
+			easing: 'ease-in-out'
+		});
+		// Increment index or wrap around to beginning again
+		currentWordIndex = (currentWordIndex + 1) % words.length;
+
+		const animation = getRandomAnimation();
+		words?.item(currentWordIndex)?.animate(animation.animation, {
+			duration: 750,
+			delay: 2000,
+			fill: 'forwards',
+			easing: 'ease-in-out'
+		});
+		undoAnimation = animation.undoAnimation;
+		timeout = setTimeout(doAnimation, lingerDuration);
+	}
+</script>
+
+<section class=" min-h-screen flex flex-col text-3xl md:text-5xl pt-12 md:pt-20">
+	<div class="flex justify-center">
+		<div>Riley is</div>
+	</div>
+	<div id="words" class="words text-5xl md:text-9xl text-center">
+		<div>EXTRAORDINARY</div>
+		<div>AMAZING</div>
+		<div>BEAUTIFUL</div>
+		<div>WONDERFUL</div>
+		<div>LOVELY</div>
+		<div>INCREDIBLE</div>
+		<div>GORGEOUS</div>
+		<div>SPECIAL</div>
+		<div>KIND</div>
+		<div>SWEET</div>
+		<div>PERFECT</div>
+		<div>ADORABLE</div>
+		<div>FUNNY</div>
+		<div>CARING</div>
+		<div>EXCEPTIONAL</div>
+		<div>UNIQUE</div>
+		<div>FANTASTIC</div>
+		<div>MAGNIFICENT</div>
+		<div>PRECIOUS</div>
+		<div>IRREPLACEABLE</div>
 	</div>
 </section>
 
 <style>
-	:root {
-		--text-height: 150.2px;
-	}
-	.text-8xl {
-		line-height: normal;
-	}
 	section {
 		background-color: black;
 		color: white;
 	}
 	.words {
-		height: var(--text-height);
-		overflow: hidden;
+		position: relative;
+	}
+	.words div:first-child {
+		opacity: 1;
 	}
 	.words div {
-		padding-bottom: 23px;
-		opacity: 1;
-		animation: move 60s linear infinite;
-		transform: translate(0, -60px);
-	}
-	@keyframes move {
-		0% {
-			transform: translate(0, 0);
-		}
-		4% {
-			transform: translate(0, 0);
-		}
-		5% {
-			transform: translate(0, calc(var(--text-height) * -1));
-		}
-		9% {
-			transform: translate(0, calc(var(--text-height) * -1));
-		}
-		10% {
-			transform: translate(0, calc(var(--text-height) * -2));
-		}
-		14% {
-			transform: translate(0, calc(var(--text-height) * -2));
-		}
-		15% {
-			transform: translate(0, calc(var(--text-height) * -3));
-		}
-		19% {
-			transform: translate(0, calc(var(--text-height) * -3));
-		}
-		20% {
-			transform: translate(0, calc(var(--text-height) * -4));
-		}
-		24% {
-			transform: translate(0, calc(var(--text-height) * -4));
-		}
-		25% {
-			transform: translate(0, calc(var(--text-height) * -5));
-		}
-		29% {
-			transform: translate(0, calc(var(--text-height) * -5));
-		}
-		30% {
-			transform: translate(0, calc(var(--text-height) * -6));
-		}
-		34% {
-			transform: translate(0, calc(var(--text-height) * -6));
-		}
-		35% {
-			transform: translate(0, calc(var(--text-height) * -7));
-		}
-		39% {
-			transform: translate(0, calc(var(--text-height) * -7));
-		}
-		40% {
-			transform: translate(0, calc(var(--text-height) * -8));
-		}
-		44% {
-			transform: translate(0, calc(var(--text-height) * -8));
-		}
-		45% {
-			transform: translate(0, calc(var(--text-height) * -9));
-		}
-		49% {
-			transform: translate(0, calc(var(--text-height) * -9));
-		}
-		50% {
-			transform: translate(0, calc(var(--text-height) * -10));
-		}
-		54% {
-			transform: translate(0, calc(var(--text-height) * -10));
-		}
-		55% {
-			transform: translate(0, calc(var(--text-height) * -11));
-		}
-		59% {
-			transform: translate(0, calc(var(--text-height) * -11));
-		}
-		60% {
-			transform: translate(0, calc(var(--text-height) * -12));
-		}
-		64% {
-			transform: translate(0, calc(var(--text-height) * -12));
-		}
-		65% {
-			transform: translate(0, calc(var(--text-height) * -13));
-		}
-		69% {
-			transform: translate(0, calc(var(--text-height) * -13));
-		}
-		70% {
-			transform: translate(0, calc(var(--text-height) * -14));
-		}
-		74% {
-			transform: translate(0, calc(var(--text-height) * -14));
-		}
-		75% {
-			transform: translate(0, calc(var(--text-height) * -15));
-		}
-		79% {
-			transform: translate(0, calc(var(--text-height) * -15));
-		}
-		80% {
-			transform: translate(0, calc(var(--text-height) * -16));
-		}
-		84% {
-			transform: translate(0, calc(var(--text-height) * -16));
-		}
-		85% {
-			transform: translate(0, calc(var(--text-height) * -17));
-		}
-		89% {
-			transform: translate(0, calc(var(--text-height) * -17));
-		}
-		90% {
-			transform: translate(0, calc(var(--text-height) * -18));
-		}
-		94% {
-			transform: translate(0, calc(var(--text-height) * -18));
-		}
-		95% {
-			transform: translate(0, calc(var(--text-height) * -19));
-		}
-		100% {
-			transform: translate(0, calc(var(--text-height) * -19));
-		}
+		position: absolute;
+		opacity: 0;
+		left: 50%;
+		perspective: 10em;
+		transform: translate(-50%, 0%);
+		line-height: normal;
 	}
 </style>
